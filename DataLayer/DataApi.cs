74971ec01729f9d2c54fc46bdc76161d057689b4
adamski234 <adamski234@pm.me>
2023-04-07 15:58:18 +0200

    Added DataApi class

---
+++ DataLayer/DataApi.cs
@@ -0,0 +1,20 @@
+﻿namespace DataLayer;
+
+public abstract class AbstractDataApi
+{
+    public abstract List<Ball> GetBalls();
+    internal class DataApi : AbstractDataApi
+    {
+        private readonly object mutex = new object();
+        public bool enabled = true;
+        public readonly Scene scene;
+        public DataApi(int width, int height, int ballCount, int ballRadius)
+        {
+            this.scene = new Scene(width, height, ballRadius, ballCount);
+        }
+        public override List<Ball> GetBalls()
+        {
+            return scene.Balls;
+        }
+    }
+}
