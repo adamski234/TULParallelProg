65806827b9841581690cedb9b9a3daf200d463b3
adamski234 <adamski234@pm.me>
2023-04-07 14:28:38 +0200

    Added Ball class

---
+++ DataLayer/Ball.cs
@@ -0,0 +1,67 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DataLayer;
+
+public class Ball : INotifyPropertyChanged
+{
+    private double x;
+    private double y;
+    private double radius;
+    public double speedX;
+    public double speedY;
+
+    public Ball(double x, double y, double radius)
+    {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        Random source = new Random();
+        do
+        {
+            this.speedX = source.NextDouble();
+        } while (speedX == 0);
+        do
+        {
+            this.speedY = source.NextDouble();
+        } while (speedY == 0);
+    }
+
+    public double X
+    {
+        get { return this.x; }
+        set
+        {
+            this.x = value;
+            OnPropertyChanged("X");
+        }
+
+    }
+
+    public double Y
+    {
+        get { return this.y;  }
+        set
+        {
+            this.y = value;
+            OnPropertyChanged("Y");
+        }
+    }
+
+    public void move()
+    {
+        this.X += this.speedX;
+        this.Y += this.speedY;
+    }
+
+
+    #region INotifyPropertyChanged
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
+}
